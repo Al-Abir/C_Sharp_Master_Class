57507f0365001d43b11fd914269b684a4d9394f6
Al-Abir <sheikhabir196@gmail.com>
2026-04-10 16:30:09 +0600

    first commit

---
+++ Working_with_BasicData_Types/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+class Program
+{
+   static void Main()
+   {
+       Console.Write("Enter your name: ");
+       string name = Console.ReadLine();
+
+       Console.Write("Enter your age: ");
+       int age = int.Parse(Console.ReadLine());
+
+       Console.WriteLine("Name: " + name);
+       Console.WriteLine("Age: " + age);
+       int[] marks = {80, 90, 85};
+       Console.WriteLine("First mark: " + marks[0]);
+   }
+}
