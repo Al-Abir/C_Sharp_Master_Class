f3c9c367592de12fda9171ebe97a8cefe8913630
Al-Abir <sheikhabir196@gmail.com>
2026-04-15 15:53:39 +0600

    Operation Text

---
+++ Operation_Text/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //  Input String
+        string text = "  abir123  ";
+
+        // ================= String Methods =================
+        string cleanText = text.Trim();              // space remove
+        string upperText = cleanText.ToUpper();      // uppercase
+
+        // ================= Character Methods =================
+        char firstChar = cleanText[0];
+
+        bool isLetter = char.IsLetter(firstChar);    // check first char letter?
+        bool isDigit = char.IsDigit(cleanText[4]);   // check number
+
+        // ================= String Formatting =================
+        Console.WriteLine($"Original Text : '{text}'");
+        Console.WriteLine($"Clean Text    : '{cleanText}'");
+        Console.WriteLine($"Upper Text    : '{upperText}'");
+
+        Console.WriteLine($"First Char '{firstChar}' is Letter? {isLetter}");
+        Console.WriteLine($"Contains Digit at index 4? {isDigit}");
+    }
+}
