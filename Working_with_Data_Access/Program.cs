5db996f8ac0ea00c5a3e95d0b05a72a7050cd05e
Al-Abir <sheikhabir196@gmail.com>
2026-04-25 15:09:28 +0600

    Data access

---
+++ Working_with_Data_Access/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Npgsql;
+
+class Program
+{
+    static void Main()
+    {
+        string connString = "Host=localhost;Username=postgres;Password=abir1981;Database=TestDB";
+
+        using var conn = new NpgsqlConnection(connString);
+        conn.Open();
+
+        // INSERT
+        var insert = new NpgsqlCommand(
+            "INSERT INTO users (name, age) VALUES ('Abir', 25)", conn);
+        insert.ExecuteNonQuery();
+
+
+     var update = new NpgsqlCommand(
+     "UPDATE users SET age = 30 WHERE name = 'Abir'", conn);
+    update.ExecuteNonQuery();
+        // READ
+        var cmd = new NpgsqlCommand("SELECT * FROM users", conn);
+        var reader = cmd.ExecuteReader();
+
+
+       
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader["id"]} - {reader["name"]} - {reader["age"]}");
+        }
+    }
+}
